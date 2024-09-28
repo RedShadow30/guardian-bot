a7b3245613edd30bd47a94c2b89ff24156dfc5aa
RedShadow30 <148815559+RedShadow30@users.noreply.github.com>
2024-09-28 12:59:43 -0500

    Set initial region to DFW

--- screens/Home/Home.jsx
+++ screens/Home/Home.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet, View, TouchableOpacity, Text, Image } from 'react-native';
 import { Icon } from "react-native-vector-icons/MaterialCommunityIcons";
+
+// Initial Region set to Dallas-Fort Worth
+const INITIAL_REGION = {
+    latitude:  32.7767,
+    longitude: -96.7970,
+    latitudeDelta: 2,
+    longitudeDelta: 0.5,
+}
 
 function Home() {
     console.log("Home Screen reached!");
     return (
         <View style={styles.container}>
-            <MapView style={styles.map} />
+            <MapView 
+                style={styles.map}
+                initialRegion={INITIAL_REGION}
+            >
+            </MapView>
         </View>
     );
 }
