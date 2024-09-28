9089bdfeac813b810690c4444677e1dfc403c58d
RedShadow30 <148815559+RedShadow30@users.noreply.github.com>
2024-09-28 12:31:29 -0500

    Added MapView

--- screens/Home/Home.jsx
+++ screens/Home/Home.jsx
@@ -1,21 +1,25 @@
-import { Text, View, StyleSheet } from "react-native";
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View, TouchableOpacity, Text, Image } from 'react-native';
 import { Icon } from "react-native-vector-icons/MaterialCommunityIcons";
-import { TouchableOpacity } from "react-native";
 
 function Home() {
     console.log("Home Screen reached!");
     return (
-        <View>
-            <Text style={styles.headingStyle}>Home Page</Text>
+        <View style={styles.container}>
+            <MapView style={styles.map} />
         </View>
     );
 }
 
+/* Below is styling the map view and map markers for testing purposes. Can be adjusted by designer. */
 const styles = StyleSheet.create({
-    /* Below is just for testing purposes, can be removed by designer */
-    headingStyle:{
-        textAlign: 'center',
-        marginTop: 100,
+    container: {
+        flex: 1,
+    },
+    map: {
+        width: '100%',
+        height: '100%',
     },
 });
 
