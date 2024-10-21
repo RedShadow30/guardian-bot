f7f9dacc04b18774a00b7a5be357125d2fd4cfb3
RedShadow30 <148815559+RedShadow30@users.noreply.github.com>
2024-10-21 15:25:10 -0500

    Configured backend profile route. Created file for styles and Profile schema.

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const register = require('./routes/register');
 const auth = require('./routes/auth');
+const profile = require('./routes/profile');
+
 const cors = require('cors');
 // Create instance of express module to represent our app
 const app = express();
 app.use(cors()); 
   
 // Establish connection to MongoDB
-
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@gb-usercluster.gihqc.mongodb.net/`, {dbName: `${process.env.DB_NAME}`})
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.log('Could not connect to MongoDB...'));
@@ -16,6 +17,7 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD
 app.use(express.json());
 app.use('/api/register', register); 
 app.use('/api/auth', auth); 
+app.use('/api/profile', profile);
 
 app.listen(3000, () => {
     console.log(`Server started on port ${process.env.PORT}...`);
