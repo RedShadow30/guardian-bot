a315b281e988ed1816638825d8574ab2a71c4877
RedShadow30 <148815559+RedShadow30@users.noreply.github.com>
2024-12-01 21:42:18 -0600

    Improved AI Chat styling

--- screens/AIScreen/AI.jsx
+++ screens/AIScreen/AI.jsx
@@ -1,106 +1,153 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { useFocusEffect } from '@react-navigation/native';
 import { View, Text, TextInput, Button, FlatList, KeyboardAvoidingView, Platform } from 'react-native';
 import styles from './styles'; // Importing styles from the styles.js file
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { Bubble, GiftedChat, InputToolbar, Send } from 'react-native-gifted-chat';
 import axios from 'axios';
 
 function AI() {
   const [messages, setMessages] = useState([]); // Chat messages state
-  const [inputText, setInputText] = useState(''); // Input text state
+  const [isAgentTyping, setIsAgentTyping] = useState(false); // Agent typing state
 
+  // Reset messages using useFocusEffect
   useFocusEffect(
-    React.useCallback(() => {
-      const welcomeMsg = { id: Math.random().toString(), text: "Hello, how may I assist you?", sender: "agent"};
-      setMessages([welcomeMsg]);
-    }, []) // empty dependency array ensure this runs every time user navigates to this page
-  );
 
-  const sendMessage = async () => {
-    if (inputText.trim()) {
-      setMessages(prevMessages => [
-        ...prevMessages,
-        { id: Math.random().toString(), text: inputText, sender: "user" }, // Add message to chat
-      ]);
-      setInputText(''); // Clear input field after sending
-    }
+    useCallback(() => {
+    // Default message to display
+    setMessages([
+      {
+        _id: 1,
+        text: "Hey there, how may I assist you?",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'GuardianBot',
+          avatar: require('../../assets/GB_IMG2.png'),
+        },
+      },
+    ])
+  }, [])
+);
 
-    // Send the message to backend
-    console.log('Frontend sending: ' + inputText);
+  const onSend = useCallback(async (messages = []) => {
+    // Add user message to chat    
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages));
+
+    // Extract user's text message
+    const userMessage = messages[0].text;
+    console.log('Frontend sending: ' + userMessage);
+
     try {
-      // Send backend request in a different port
+      // Set Agent typing state to true
+      setIsAgentTyping(true);
+
+      // Send backend request in DIFFERENT port (3002)
       const response = await axios.post('http://${REPLACE_IP_HERE}:3002/rag', {
-        data: {
-          message: inputText
-        }
+        data: { message: userMessage }
       })
   
-       if(response.status == 200) {
-        console.log('Response successful');
-        // Display the agent response in console
-        console.log('Agent Response: ' + response.data?.data?.content);
+      if(response.status == 200) {
+        // Store and display Agent response
         const agentResponse = response.data?.data?.content || 'Sorry, I am unable to answer that.';
+        console.log('Agent Response: ' + agentResponse);
 
-        if(agentResponse) {
-          // Add agent response to chat messages
-          setMessages(prevMessages => [
-            ...prevMessages,
-            { id: Math.random().toString(), text: response.data?.data?.content, sender: "agent"},
-          ]);
-        }
-        else {
-          console.error('Agent response is undefined');
-        }
-
-       }
-       else {
+        // Add Agent Message to chat
+        setMessages(previousMessages => 
+          GiftedChat.append(previousMessages, [
+            {
+              _id: Math.random().toString(),
+              text: agentResponse,
+              createdAt: new Date(),
+              user: {
+                _id: 2, // ID of Agent
+                name: 'GuardianBot',
+                avatar: require('../../assets/GB_IMG2.png'),
+              },
+            },
+          ])
+        );
+      }
+      else {
         console.log('Response unsuccessful');
-        // Display agent response as 'Sorry, I am unable to answer that.'
-       }
+      }
     }
     catch(err) {
       console.error('Error communicating with backend: ', err);
     }
+    // Hide typing indicator after response returned
+    setIsAgentTyping(false);
+  }, [])
+
+  // Render typing box
+  const renderInputToolbar = (props) => {
+    return (
+      <InputToolbar
+        {...props}
+        containerStyle={styles.inputBar}
+      />
+    )
   };
 
-  const renderMessage = ({ item }) => (
-    // Color message based on who sent it
-    <View style={[styles.messageContainer, item.sender === "agent"? styles.agentMessage : styles.userMessage]}>
-      {/* Place respective icon based on who sent it */}
-      {item.sender === "agent" && (
-        <Icon name="rocket" size={25} style={styles.icon}/>
-      )}
-      {/* Change color of text to white if user message*/}
-      <Text style={[styles.messageText, item.sender === "user" && { color: '#fff' }]}>{item.text}</Text>
-      {item.sender === "user" && (
-        <Icon name="user" size={30} style={styles.icon} />
-      )}
-    </View>
-  );
+  // Add a send icon
+  const renderSend = (props) => {
+    return (
+      <Send {...props}>
+        <View style={styles.sendIcon}>
+          <Icon name="send" size={24} color="#0075FD" />
+        </View>
+      </Send>
+    )
+  }
+
+  // Render the chat bubble colors
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={styles.bubbleWrapper}
+        />
+    );
+  };
+
+  const CustomScroll = () => {
+    return (
+      <View>
+        <Icon name="arrow-down" size={25} color={"#000000"} />
+      </View>
+    );
+  };
+
+  const renderFooter = () => {
+    if(isAgentTyping) {
+      return (
+      <View style={styles.footer}>
+        <Text style={styles.typingText}>Agent thinking...</Text>
+      </View>
+    
+    )
+    }
+    return null;
+  };
 
   return (
-    <KeyboardAvoidingView 
-      behavior={Platform.OS === "ios" ? "padding" : "height"} 
-      style={styles.container}
-      keyboardVerticalOffset={5} // Shifts input box above keyboard
-    >
-      {/* Chat messages */}
-      <FlatList
-        data={messages}
-        keyExtractor={item => item.id}
-        renderItem={renderMessage}
-        style={styles.chatContainer}
-      />
-
-      {/* Input and send button */}
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          value={inputText}
-          onChangeText={setInputText}
-          placeholder="Type a message..."
+    <KeyboardAvoidingView style={styles.container}>
+      <View style={styles.container}>
+        {/* Chat messages */}
+        <GiftedChat
+          messages={messages}
+          onSend={messages => onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+          scrollToBottom={true}
+          renderInputToolbar={renderInputToolbar}
+          renderSend={renderSend}
+          renderBubble={renderBubble}
+          scrollToBottomComponent={CustomScroll}
+          isTyping={isAgentTyping}
+          renderFooter={renderFooter}
         />
-        <Button title="Send" onPress={sendMessage} />
       </View>
     </KeyboardAvoidingView>
   );
--- screens/AIScreen/styles.js
+++ screens/AIScreen/styles.js
@@ -4,67 +4,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'space-between',
+    marginBottom: 60,
   },
-  chatContainer: {
-    flex: 1,
-    padding: 10,
-  },
-  messageContainer: {
-    padding: 9,
-    paddingEnd: 13,
-    backgroundColor: '#D0D0D0',
-    borderRadius: 10,
-    marginVertical: 5,
+  inputBar: {
+    borderRadius: 16,
     marginHorizontal: 8,
-    flexDirection: 'row', // align icon and text in a row
-    alignItems: 'center', // center vertically
-    elevation: 5,
-    shadowColor: '#000', // shadow for Android
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.3,
-    shadowRadius: 4,
-  },
-  agentMessage: {
-    backgroundColor: '#D0D0D0',
-    alignSelf: 'flex-start', // align agent messages to left
-    marginRight: 50,
-  },
-  userMessage: {
-    backgroundColor: '#0084FF',
-    alignSelf: 'flex-end', // align user messages to the right
-    paddingStart: 15,
-    paddingEnd: 5,
-  },
-  messageText: {
-    color: '#000',
-    fontSize: 20,
-    maxWidth: '100%'
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    padding: 2,
-    alignItems: 'center',
-    marginBottom: 110, // for the input box to be above bottom tabs
-    marginTop: 10,
-  },
-  input: {
-    flex: 1,
-    borderColor: 'black',
-    borderWidth: 1,
-    borderRadius: 20,
-    paddingHorizontal: 15,
-    paddingVertical:15,
-    marginRight: 10,
-    fontSize:15,
-  },
-  icon: {
-    width: 30,
-    height: 30,
-    marginHorizontal: 6,
     marginTop: 5,
+    borderTopWidth: 0,
+  },
+  sendIcon: {
+    margin:9, 
+    marginBottom: 13, 
+    marginTop: 3
+  },
+  bubbleWrapper: {
+    left: {
+      backgroundColor: '#e6e7e8',
+    },
+    right: {
+      backgroundColor: '#3086fe',
+    },
+  },
+  footer: {
+    margin: 10,
+  },
+  typingText: {
+    fontStyle:'italic', 
+    color: 'gray'
   }
 });
 
